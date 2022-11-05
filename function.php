34a68a0f12aeea8a04a4455cdbfb2100e1dab2e6
mallexibra <mallexibra@gmail.com>
2022-11-05 08:57:23 +0700

    Melanjutkan belajar PHP bagian fungsi Time

--- function.php
+++ function.php
@@ -12,5 +12,16 @@
 // Time
 // UNIX Time / EPOCH TimeStamp
 // echo time();
-echo date("l, d-M-Y", time()+60*60*24*10) // Mencari 10 hari selanjutya;
+// echo date("l, d-M-Y", time()+60*60*24*10) // Mencari 10 hari selanjutya;
+// echo date("l, d-M-Y", time()-60*60*24*10) // Mencari 10 hari sebelumnya;
+
+// mktime
+// Membuat detik waktu sendiri;
+// mktime(0, 0, 0, 0, 0, 0);
+// mktime(Jam, Menit, Detik, Bulan, Hari, Tahun);
+// mktime(0, 0, 0, 04, 30, 2004);
+// penggabungan date() dan mktime() untuk mencari waktu yang diinginkan sesuai waktu tertentu;
+// echo date("l", mktime(0,0,0,8, 03, 2022));
+
+
 ?>
