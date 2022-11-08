c50c3a841f4a34ea3aca6c790a63e89cefb9ceee
mallexibra <mallexibra@gmail.com>
2022-11-08 20:53:40 +0700

    Melanjutkan belajar function dan Belajar Materi baru yaitu Array

---
+++ array.php
@@ -0,0 +1,41 @@
+<?php
+// ARRAY
+// Array merupakan sebuah variabel yang dapat menampung banyak nilai
+// Array boleh berisi banyak tipe data
+// Array adalah pasangan antara Key dan Value;
+// Key nya adalah index yang dimulai dari 0;
+// Value nya adalah nilai dari tiap indexnya;
+// ! Pembuatan Array Lama
+// $array1 = array("Senin", "Selasa", "Rabu");
+// ! Pembuatan Array Baru
+// $array2 = ["Januari", "Februari", "Maret"];
+// * Mencetak Array
+// var_dump() / print_r()
+// var_dump($array1);
+// print_r($array2);
+// Menampilkan1 elemen Array
+// echo $array2[0];
+// Menambahkan sebuah nilai pada array di bagian akhir;
+// $array2[] = "April";
+// print_r($array2);
+
+// Pengulangan pada Array
+// Bisa menggunakan for / foreach;
+// for($i = 0; $i < count($array2); $i++){
+//     echo $array2[$i];
+// }
+
+// foreach($array2 as $arr){
+//     echo $arr;
+// }
+
+// Contoh Array didalam Array;
+// $mahasiswa = [["Maulana", "Malik", "Ibrahim"], ["Anastasya", "Afianti", "Y.R"]];
+
+// print_r($mahasiswa[1][0]);
+// echo "<br>";
+// foreach($mahasiswa as $mhs){
+//     echo $mhs[0];
+// }
+
+?>
--- function.php
+++ function.php
@@ -23,5 +23,40 @@
 // penggabungan date() dan mktime() untuk mencari waktu yang diinginkan sesuai waktu tertentu;
 // echo date("l", mktime(0,0,0,8, 03, 2022));
 
+// strtotime
+// Membuat waktu menggunakan tanggal yang telah ditentukan sendiri
+// echo date("l", strtotime("30 maret 2004"));
 
+// strlen
+// echo strlen("Hallo");
+// strlen() akan mengembalikan sebuah nilai berupa angka dari panjangnya string yang di inputkan;
+
+// strcmp
+// echo strcmp("Hallo", "hallo semuanya");
+// strcmp() akan membandingkan 2 buah string dan akan mengembalikan nilai berupa angka:
+// 0 jika kedua string itu sama
+// - jika string1 kurang dari string2
+// + jika string1 lebih dari string2
+// Jangan lupa kalo strcmp() ini juga case-sensitive;
+
+// explode
+// explode(separator, string, limit);
+// separator(required): bagian mana yang akan di pecah/split.
+// string(required): string yang mana yang akan dipecah, biasanya menggunakan variabel.
+// limit(optional): limit dari panjang array
+// $str = "one, two, three, four";
+// print_r(explode(',',$str,5));
+
+// htmlspecialchars()
+// $str = "<b>Bold</b>";
+// echo htmlspecialchars($str);
+// htmlspecialchars() akan mengubah code html yang akan menjadi output sesuai sifat dari elemen tersebut menjadi sebuah string biasa;
+
+// User-Defined Function
+// Fungsi yang dibuat oleh user sendiri.
+function salam($akhirnama = "ibrahim"){
+    $coba = "Maulana malik $akhirnama";
+    return $coba;
+}
+echo salam("Ganteng");
 ?>
